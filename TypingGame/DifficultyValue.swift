099d90adf73aa611e1a600af4d6c73fc787295e6
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2023-03-21 10:18:46 +0100

    first commit

---
+++ TypingGame/DifficultyValue.swift
@@ -0,0 +1,12 @@
+//
+//  DifficultyValue.swift
+//  TypingGame
+//
+//  Created by Kanyaton Somjit on 2023-03-20.
+//
+
+import Foundation
+
+class DifficultyValue {
+    
+}
--- TypingGame/ViewController.swift
+++ TypingGame/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+   
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet var designButton: UIButton!
+    
+    @IBOutlet weak var chooseDifficultyValue: UIPickerView!
+    var difficulty = [String]()
+    
+    let segueIdGoToGame = "goToGame"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        welcomeLabel.text = "Welcome to\ntyping game"
+        buttonDesign()
+        
+    //choice for difficulty.
+        difficulty.append("EASY")
+        difficulty.append("MEDIUM")
+        difficulty.append("HARD")
+        
     }
-
+    
+    
+    @IBAction func goToGameButton(_ sender: UIButton) {
+         performSegue(withIdentifier: segueIdGoToGame, sender: self)
+    }
+    
+    func buttonDesign(){
+        designButton.layer.cornerRadius = 15
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return difficulty.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return "\(difficulty[row])"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+       // print("\(row) : \(difficulty[row])")
+    }
 
 }
 
